91e8e7066eee99fd80816cb4e1a91b8604d93034
NayraBH <nayrabhernandez@gmail.com>
2022-04-24 10:51:32 +0000

    feat: star wars blog API with python flask

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,75 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    list_people = People.get_people()
+    
+    return jsonify(list_people), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<people_id>', methods=['GET'])
+def get_people_id(people_id):
+    people = People.query.get(people_id)
 
-    return jsonify(response_body), 200
+    return jsonify(people.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    list_planets = Planet.get_planets()
+
+    return jsonify(list_planets), 200
+
+@app.route('/planets/<planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+
+    return jsonify(planet.serialize()), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    list_users= User.get_users()
+
+    return jsonify(list_users), 200
+
+@app.route('/favorites/<user_id>', methods=['GET'])
+def get_user_favorites(user_id):
+    list_favorites = Favorites.get_favorites(user_id)
+
+    return jsonify(list_favorites), 200
+
+@app.route('/favorite/planet/<planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    body = request.get_json()
+    new_favorite = Favorites(user_id=body["user_id"], planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify("Favorite planet added"), 201
+
+@app.route('/favorite/people/<people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    body = request.get_json()
+    new_favorite = Favorites(user_id=body["user_id"], people_id=people_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify("Favorite people added"), 201
+
+@app.route('/favorite/planet/<planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    body = request.get_json()
+    favorite = Favorites.query.filter(Favorites.user_id==body['user_id'], Favorites.planet_id==planet_id).first()
+    if favorite is not None:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify("Favorite planet deleted"), 200
+
+@app.route('/favorite/people/<people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    body = request.get_json()
+    favorite = Favorites.query.filter(Favorites.user_id==body['user_id'], Favorites.people_id==people_id).first()
+    if favorite is not None:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify("Favorite people deleted"), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,104 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+    def get_users():
+        users = db.session.query(User)
+        list_users = []
+        for user in users:
+            list_users.append(user.serialize())
+        return list_users
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.Integer)
+    gender = db.Column(db.String(120))
+    height = db.Column(db.String(120))
+    skin_color = db.Column(db.String(120))
+    eye_color = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+        }
+
+    def get_people():
+        people = People.query.all()
+        list_people = []
+        for character in people:
+            list_people.append(character.serialize())
+        return list_people
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(120))
+    population = db.Column(db.String(120))
+    orbital_period = db.Column(db.String(120))
+    rotation_period = db.Column(db.String(120))
+    diameter = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+        }
+    
+    def get_planets():
+        planets = Planet.query.all()
+        list_planets = []
+        for planet in planets:
+            list_planets.append(planet.serialize())
+        return list_planets
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    user = db.relationship(User)
+    people = db.relationship(People)
+    planet = db.relationship(Planet)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+        }
+
+    def get_favorites(user_id):
+        favorites = db.session.query(Favorites).filter(Favorites.user_id == user_id)
+        list_favorites = []
+        for favorite in favorites:
+            list_favorites.append(favorite.serialize())
+        return list_favorites
+
+    
+
